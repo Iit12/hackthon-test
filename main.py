2ba4e61ba29dbf1687c278356ba661a8ab731fef
Iit12 <168269955+Iit12@users.noreply.github.com>
2026-04-24 23:53:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# This is the "Magic" part that fixes the white screen
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # This allows any website to talk to your backend
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
